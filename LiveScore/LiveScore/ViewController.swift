c7e1a24eca48bd5883ca1ef76e785ccfab6beea8
Macbook <48622449+datto97@users.noreply.github.com>
2024-01-19 08:29:26 +0700

    create project

---
+++ LiveScore/LiveScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveScore
+//
+//  Created by Macbook on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
