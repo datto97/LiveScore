6b37bbd5bc40bda54a8b1db00eea6cd8edfafb44
Macbook <48622449+datto97@users.noreply.github.com>
2024-01-20 11:11:00 +0700

    add asset

---
+++ LiveScore/LiveScore/Resource/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  LiveScore
+//
+//  Created by Macbook on 1/20/24.
+//
+
+import UIKit
+
+struct AppColors{
+    static let White = UIColor(named: "White")
+    static let Black22 = UIColor(named: "Black22")
+    static let Black18 = UIColor(named: "Black18")
+    static let Grey65 = UIColor(named: "Grey65")
+    static let GreyC4 = UIColor(named: "GreyC4")
+    static let Blue24 = UIColor(named: "Blue24")
+}
