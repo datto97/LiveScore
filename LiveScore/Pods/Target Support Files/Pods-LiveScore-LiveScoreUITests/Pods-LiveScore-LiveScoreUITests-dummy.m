19a298d07a5ed203e490b4f75cd174639f2cf39b
Macbook <48622449+datto97@users.noreply.github.com>
2024-01-20 11:12:00 +0700

    Install pod

---
+++ LiveScore/Pods/Target Support Files/Pods-LiveScore-LiveScoreUITests/Pods-LiveScore-LiveScoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiveScore_LiveScoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LiveScore_LiveScoreUITests
+@end
---
+++ LiveScore/Pods/Target Support Files/Pods-LiveScoreTests/Pods-LiveScoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiveScoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_LiveScoreTests
+@end
